3c21d3e4514bb1f2608136f217045edd01b95e4c
pradnya1994 <pradnya.unavene2529@gmail.com>
2024-05-23 19:16:25 +0530

    Division of two numbers by static method calling

---
+++ DivisionOfNumbers.java
@@ -0,0 +1,18 @@
+package assignment2_MethodCallingDivision;
+
+public class DivisionOfNumbers {
+	
+/*
+ this is static method having return type integer. int a and int b are the arguments we are passing 
+ to store the values and do the operation. 
+ We have written return a+b statement because we have taken return type of method as integer.
+ */
+	public static int Division(int a, int b)
+	{
+		return a/b;
+	}
+
+
+	}
+
+
